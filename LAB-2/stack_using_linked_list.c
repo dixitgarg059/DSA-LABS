e0b5acc08e5167f973c87e7765f6d8fffb03b595
dhruvsharma410 <sharmadhruv410@gmail.com>
2021-03-03 10:54:26 -0100

    added stack using linked list

---
+++ LAB-2/stack_using_linked_list.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// push, pop, top, display, empty 
+
+struct node {
+	int data;
+	struct node* next;
+};
+
+struct node * head;
+
+void push(int val) //1
+{
+	struct node * temp;
+	temp = (struct node*)malloc(sizeof(struct node));
+	temp -> data = val;
+	temp -> next = head;
+	head = temp;
+}
+
+void pop()
+{
+	head = head->next;
+}
+
+int top()
+{
+	if(head==NULL)
+	{
+		printf("Underflow\n");
+		return -1;
+	}
+	else
+		return head->data;
+}
+
+void display()
+{
+	struct node* walk = head;
+	while(head!=NULL)
+	{
+		printf("%d\n",head->data);
+		head = head->next;
+	}
+	head = walk;
+}
+void isempty()
+{
+	if(head==NULL)
+	{
+		printf("Stack is empty\n");
+	}
+	else
+		printf("Stack not empty\n");
+}
+int main()
+{
+	push(11);
+	push(5);
+	display();
+	pop();
+	isempty();
+	pop();
+	display();
+	isempty();
+}
